41414181ccfdfcf7896ac8f282a9777de9791738
jordan.schoen <jordan.schoen@bodybuilding.com>
2015-03-06 21:22:01 -0700

    added facebook id

--- Appathon/www/js/app.js
+++ Appathon/www/js/app.js
@@ -2,7 +2,7 @@ angular.module('sociogram', ['ionic', 'openfb', 'sociogram.controllers'])
 
     .run(function ($rootScope, $state, $ionicPlatform, $window, OpenFB) {
 
-        OpenFB.init('YOUR_FB_APP_ID');
+        OpenFB.init('1615618795335027');
 
         $ionicPlatform.ready(function () {
             if (window.StatusBar) {
