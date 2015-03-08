e2b2ed04316969f4d7dbe7e229c8a5e27e91f28f
jordan.schoen <jordan.schoen@bodybuilding.com>
2015-03-07 21:45:45 -0700

    fixing model

--- server/app/models/model.js
+++ server/app/models/model.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // define the schema for our user model
 var postSchema = mongoose.Schema({
         text : String,
-        path: String,
+        image: String,
         done : Boolean
     });
