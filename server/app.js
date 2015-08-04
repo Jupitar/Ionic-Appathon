07aef59225c3066124d7008a48def6a49c9e1289
Jordan Schoen <jordan@fierysolid.com>
2015-08-04 17:40:25 -0600

    A bunch of sexy changes

--- server/app.js
+++ server/app.js
@@ -2,17 +2,15 @@ var express = require("express"),
     app = express(),
     formidable = require('formidable'),
     util = require('util')
-    fs   = require('fs-extra'),
-    qt   = require('quickthumb');
-var mongoose = require('mongoose');
-var flash    = require('connect-flash');
-
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
-var configDB = require('./config/database.js');
+    fs = require('fs-extra'),
+    qt = require('quickthumb'),
+    mongoose = require('mongoose'),
+    flash = require('connect-flash'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    configDB = require('./config/database.js');
 
 mongoose.connect(configDB.url);
 
@@ -20,7 +18,6 @@ app.use(qt.static(__dirname + '/'));
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser());
-
 app.use(session({ secret: 'america' }));
 app.use(flash());
 
